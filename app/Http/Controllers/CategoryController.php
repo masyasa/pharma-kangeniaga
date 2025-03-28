edf968122ae96b138989b6259c9572966da0ba04
Salistia Putra <masyasa2021@gmail.com>
2025-03-29 01:45:11 +0700

    mencoba melengkapi crud categori: baru index dan create

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,7 +13,8 @@ class CategoryController extends Controller
     public function index()
     {
         //
-        return view('admin.categories.index');
+        $categories = Category::all();
+        return view('admin.categories.index', ['categories' => $categories]);
     }
 
     /**
