d351012f9bf46ba7ae340e30ae9f701ca0fba507
Salistia Putra <masyasa2021@gmail.com>
2025-04-01 07:44:00 +0700

    memperbaiki form add category

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -11,15 +11,23 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{-- {{ __("You're logged in!") }} --}}
                     {{-- @livewire('form-add-category') --}}
-                    <form action="{{ route('admin.categories.store') }}" method="POST">
-                        @csrf
-                        <div><label for="name" class="form-label">Nama :</label><input class="form-control"
-                                type="text" name="name"></div>
-                        <div><label for="icon" class="form-label">Icon :</label><input class="form-control"
-                                type="text" name="icon"></div>
+                    <div class="w-full max-w-lg p-6 bg-white rounded-lg shadow-md">
+                        <form action="{{ route('admin.categories.store') }}" method="POST">
+                            @csrf
+                            <div><label for="name" class="block text-sm font-medium text-gray-600">Nama
+                                    :</label><input
+                                    class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                    type="text" name="name"></div>
+                            <div><label for="icon" class="block text-sm font-medium text-gray-600">Icon
+                                    :</label><input
+                                    class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                    type="text" name="icon"></div>
+                            <div class="text-center "><button type="submit"
+                                    class="mt-4 w-56 px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600">Tambah
+                                    Category</button></div>
 
-                        <button type="submit" class="btn btn-primary">Tambah Category</button>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
